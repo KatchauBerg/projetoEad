f4144ee20d4a05fa5dbdb1f3a91f2f5255c4da4a
KatchauBerg <kevinlima138@gmail.com>
2022-06-02 17:12:23 -0300

    Subindo Arquivos Atualizados

--- application/controllers/Respostas.php
+++ application/controllers/Respostas.php
@@ -7,42 +7,4 @@ class respostas extends CI_Controller
 	{
 		$this->load->view('pages/aluno/respostas');
 	}
-
-	public function enviaDadosLogin()
-	{
-		$getDadosLogin = $this->input->post();
-
-		$x = $this->validaLogin($getDadosLogin['ra'], $getDadosLogin['senha']);
-		print_r($x);
-
-
-	}
-
-	public function validaLogin($ra, $senhaUsuario)
-	{
-
-		$query = $this->db->get('mdctable')->result_array();
-
-
-		$query = (object) $query;
-		foreach ($query as $dadosUsuario)
-		{
-			$dadosUsuario = (object) $dadosUsuario;
-			// var_dump([$dadosUsuario->Senha, $senhaUsuario]); exit;
-
-			if($ra == $dadosUsuario->Ra_aluno &&  $senhaUsuario == $dadosUsuario->Senha)
-			{
-				// print_r($dadosUsuario->validaUsuario); exit;
-				if($dadosUsuario->validaUsuario == '1')
-				{
-					echo "Bem vindo Professor";
-				}
-			}
-			else
-			{
-				echo "Erro ao realizar o Login, Verifique Seus Dados";
-				return;
-			}
-		}
-	}
 }
--- application/controllers/reclamaLista.php
+++ application/controllers/reclamaLista.php
@@ -8,42 +8,4 @@ class reclamaLista extends CI_Controller
 		$this->load->view('pages/professor/reclamaLista');
 
 	}
-
-	public function dadosCadastro()
-	{
-		$getDadosCadastro = $this->input->post();
-
-		$nomeAluno = $getDadosCadastro['nome'];
-		$sobreNomeAluno = $getDadosCadastro['sobreNome'];
-		$raAluno = $getDadosCadastro['ra'];
-		$isAluno = $getDadosCadastro['valorOption'];
-		$senhaAluno = $getDadosCadastro['senha'];
-		$emailAluno = $getDadosCadastro['email'];
-
-		$data = date('Y-m-d H:i:s');
-		// print_r($data); exit;
-
-		$this->cadastraUsuario($nomeAluno, $sobreNomeAluno, $raAluno, $isAluno,$emailAluno, $senhaAluno, $data);
-
-	}
-
-	public function cadastraUsuario($nome, $sobreNome, $ra, $isAluno, $email, $senha, $data)
-	{
-
-		$isAluno = (bool) $isAluno;
-
-		$data =
-		[
-			'Ra_aluno' => $ra,
-			'Senha' => $senha,
-			'E-mail' => $email,
-			'nome' => $nome,
-			'sobrenome' => $sobreNome,
-			'validaUsuario' => $isAluno,
-			'Data_inclusao' => $data
-		];
-
-		$sql = $this->db->insert("mdctable", $data);
-		return $sql;
-	}
 }
